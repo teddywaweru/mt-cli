10404c68675b86988a27d0a54a7fb91aba38c0bb
Teddy Njuguna <teddywaweru@gmail.com>
2024-06-08 20:18:22 -0500

    pub values to display on gui

--- mt5/src/account.rs
+++ mt5/src/account.rs
@@ -4,15 +4,15 @@ use serde_json;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Account {
-    account_number: i32,
-    name: String,
-    current_time: String,
+    pub account_number: i32,
+    pub name: String,
+    pub current_time: String,
     pub current_balance: f32,
-    current_equity: f32,
-    leverage: u32,
+    pub current_equity: f32,
+    pub leverage: u32,
     pub currency: String,
-    free_margin: f32,
-    current_profit: f32,
+    pub free_margin: f32,
+    pub current_profit: f32,
 }
 // "{'_action': 'GET_ACCOUNT_INFO', 'account_number':61189795, '_data': [{'current_time': '2024.04.19
 // 08:56', 'name':'Teddy Waweru Njuguna', 'balance':40862.08000000, 'equity':41827.15000000, 'profit':965.07000000, 'margin_free':41003.31000000,
