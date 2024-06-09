9e85de0f7fefc46664b0c55324a8a359a343aa22
Teddy Njuguna <teddywaweru@gmail.com>
2024-06-08 20:26:13 -0500

    add: init tauri backend logic integrating to mt5 api

--- gui/src-tauri/src/main.rs
+++ gui/src-tauri/src/main.rs
@@ -1,10 +1,54 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use mt5::{Account,Symbols, OrderType, OrderRequest};
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+
+#[tauri::command(rename_all = "snake_case")]
+fn update_account_info() -> Account {
+    Account::default()
+}
+
+#[tauri::command(rename_all = "snake_case")]
+fn get_active_trades() {}
+
+#[tauri::command(rename_all = "snake_case")]
+fn get_symbols() -> Symbols {
+    Symbols::default()
+}
+#[tauri::command(rename_all = "snake_case")]
+fn get_order_types() -> Vec<OrderType> {
+    OrderType::list_order_types()
+}
+
+#[tauri::command(rename_all = "snake_case")]
+fn get_instant_rates() {}
+
+#[tauri::command(rename_all = "snake_case")]
+fn calculate_order() -> OrderRequest {
+    todo!()
+
+}
+#[tauri::command(rename_all = "snake_case")]
+fn execute_instant_order(order: String) {
+    let risk = 0.02;
+
+    // let response = Mt5Bridge::generate_order(&symbol, order_type, risk);
+    todo!()
+}
+
+#[tauri::command(rename_all = "snake_case")]
+fn execute_pending_order() {}
+
+#[tauri::command(rename_all = "snake_case")]
+fn track_prices() {}
+
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
+            update_account_info,
+            get_symbols,
+            get_order_types
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
